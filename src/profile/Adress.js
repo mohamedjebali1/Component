c83c12def3328d01536ed50d445aff8282749a9a
Mohamed Jebali <60862935+mpredateur@users.noreply.github.com>
2020-11-25 22:28:06 +0100

    Add files via upload

---
+++ src/profile/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div className='adress'>
+            <h3> Kélibia, Nabeul </h3>
+        </div>
+    )
+}
+
+export default Adress 
---
+++ src/profile/ProfilPhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilPhoto = () => {
+    return (
+        <div>
+            <img src='/profil.jpg' height='400px' />
+        </div>
+    )
+  }
+  
+  export default ProfilPhoto
+  
+  
